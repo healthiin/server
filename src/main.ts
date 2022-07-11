9b5d536972e2a558355570f8d9c76a514a6a4d2b
임채성 <puleugo@gmail.com>
2022-07-12 02:35:59 +0900

    init: NestJS 환경 구축

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import { MainModule } from './main.module';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MainModule);
   await app.listen(3000);
 }
 bootstrap();
