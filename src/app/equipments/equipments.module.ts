4f9ca0cb77761f50f00841574487ff5061ddc8a9
임채성 <puleugo@gmail.com>
2022-07-12 18:23:30 +0900

    fix: import 오류 수정

--- src/app/equipments/equipments.module.ts
+++ src/app/equipments/equipments.module.ts
@@ -1,6 +1,6 @@
-
 import { EquipmentsService } from './equipments.service';
 import { EquipmentsController } from './equipments.controller';
+import { Module } from '@nestjs/common';
 
 @Module({
   providers: [EquipmentsService],
