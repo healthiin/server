d823eeec35425e50a45057c74dc7abec524cf276
Junsu Lee <me@adrinerdp.co>
2022-07-20 16:38:39 +0900

    typo: 빠진 path alias 추가

--- src/infrastructure/types/request.types.ts
+++ src/infrastructure/types/request.types.ts
@@ -1,6 +1,6 @@
 import { Request as ExpressRequest } from 'express';
 
-import { User } from '../../domain/user/user.entity';
+import { User } from '@domain/user/user.entity';
 
 export type Request = ExpressRequest & {
   user: User;
