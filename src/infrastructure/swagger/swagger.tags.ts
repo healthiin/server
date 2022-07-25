264070aa291eda153b38f20d7e2cc1c929b33dfa
Junsu Lee <me@adrinerdp.co>
2022-07-25 21:16:05 +0900

    docs: 사용자 관련 문서 정리

--- src/infrastructure/swagger/swagger.tags.ts
+++ src/infrastructure/swagger/swagger.tags.ts
@@ -1,5 +1,6 @@
 export const tags: SwaggerTag[] = [
   { name: 'Authentication', description: '사용자 인증' },
+  { name: 'User', description: '사용자 계정' },
 ];
 
 type SwaggerTag = { name: string; description: string };
