ebb720560cdab3a41456c77ca8fd986aec577954
Junsu Lee <me@adrinerdp.co>
2022-07-25 21:40:26 +0900

    docs: dto 관련 문서 정리

--- src/app/user/dtos/user-create.request.ts
+++ src/app/user/dtos/user-create.request.ts
@@ -4,37 +4,28 @@ import { IsNotEmpty, IsString } from 'class-validator';
 import { NicknameLength } from '@domain/user/user.errors';
 
 export class UserCreateRequest {
-  @ApiProperty({
-    example: 'id123123',
-    description: '아이디',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   username!: string;
 
-  @ApiProperty({
-    example: 'password123!',
-    description: '비밀번호',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password!: string;
 
-  @ApiProperty({
-    example: '홍길동',
-    description: '본명',
-  })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name!: string;
 
-  @ApiProperty({ example: '파이팅하는 무지', description: '닉네임' })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @NicknameLength({ message: `닉네임 길이가 올바르지 않습니다.` })
   nickname!: string;
 
-  @ApiProperty({ example: '01012341234', description: '전화번호' })
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   phoneNumber!: string;
