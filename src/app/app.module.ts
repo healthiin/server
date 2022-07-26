921f76783dca3bba8036da8c27fade84b592da22
임채성 <puleugo@gmail.com>
2022-07-27 00:41:57 +0900

    feat: implementation articles

--- src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { AuthModule } from '@app/auth/auth.module';
-import { UserModule } from '@app/user/user.module';
-
-@Module({
-  imports: [UserModule, AuthModule],
-})
-export class AppModule {}
