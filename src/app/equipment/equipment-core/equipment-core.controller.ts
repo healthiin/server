890012abcaeaf772748cda7b17de90dcf82159b0
puleugo <puleugo@gmail.com>
2022-08-09 20:50:13 +0900

    feat: equipment 생성 메소드 구현

--- src/app/equipment/equipment-core/equipment-core.controller.ts
+++ src/app/equipment/equipment-core/equipment-core.controller.ts
@@ -1,76 +1,23 @@
-import {
-  Body,
-  Controller,
-  DefaultValuePipe,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  ParseUUIDPipe,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common';
-import {
-  ApiNotFoundResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiQuery,
-  ApiTags,
-} from '@nestjs/swagger';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
-import { CreateEquipmentRequest } from '@app/equipment/equipment-core/dtos/create-equipment.request';
-import { EquipmentProfileResponse } from '@app/equipment/equipment-core/dtos/equipment-profile.response';
-import { UpdateEquipmentRequest } from '@app/equipment/equipment-core/dtos/update-equipment.request';
+import { CreateEquipmentCoreRequest } from '@app/equipment/equipment-core/dtos/create-equipment-core.request';
 import { EquipmentCoreService } from '@app/equipment/equipment-core/equipment-core.service';
-import { EQUIPMENT_ERRORS } from '@domain/equipment/equipment.errors';
-import { Pagination } from '@infrastructure/types/pagination.types';
 
-@Controller('equipments')
-@ApiTags('Equipment')
+@Controller('equipment')
 export class EquipmentCoreController {
   constructor(private readonly equipmentCoreService: EquipmentCoreService) {}
 
   @Get()
-  @ApiOperation({ summary: '헬스 기구를 검색합니다.' })
-  @ApiOkResponse({ type: [EquipmentProfileResponse] })
-  @ApiQuery({ name: 'page', required: false, example: 1 })
-  @ApiQuery({ name: 'limit', required: false, example: 20 })
-  @ApiNotFoundResponse({ description: EQUIPMENT_ERRORS.EQUIPMENT_NOT_FOUND })
-  async searchEquipment(
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
-    @Query('limit', new DefaultValuePipe(20), ParseIntPipe) limit: number,
-  ): Promise<Pagination<EquipmentProfileResponse>> {
-    return this.equipmentCoreService.searchEquipment(page, limit);
+  async getEquipments(): Promise<object[]> {
+    return await this.equipmentCoreService.getEquipments();
   }
 
   @Post()
-  @ApiOperation({ summary: '헬스 기구를 생성합니다.' })
-  @ApiOkResponse({ type: EquipmentProfileResponse })
-  async postEquipment(
-    @Body() data: CreateEquipmentRequest,
-  ): Promise<EquipmentProfileResponse> {
-    return this.equipmentCoreService.postEquipment(data);
-  }
-
-  @Patch(':equipmentId')
-  @ApiOperation({ summary: '헬스 기구를 수정합니다.' })
-  @ApiOkResponse({ type: EquipmentProfileResponse })
-  @ApiNotFoundResponse({ description: EQUIPMENT_ERRORS.EQUIPMENT_NOT_FOUND })
-  async updateEquipment(
-    @Param('equipmentId', ParseUUIDPipe) equipmentId: string,
-    @Body() data: UpdateEquipmentRequest,
-  ): Promise<EquipmentProfileResponse> {
-    return this.equipmentCoreService.updateEquipment(equipmentId, data);
-  }
-
-  @Delete(':equipmentId')
-  @ApiOperation({ summary: '헬스 기구를 삭제합니다.' })
-  @ApiOkResponse({ type: Boolean })
-  @ApiNotFoundResponse({ description: EQUIPMENT_ERRORS.EQUIPMENT_NOT_FOUND })
-  async deleteEquipment(
-    @Param('equipmentId', ParseUUIDPipe) equipmentId: string,
-  ): Promise<boolean> {
-    return this.equipmentCoreService.deleteEquipment(equipmentId);
+  async createEquipment(
+    @Body() createEquipmentCoreRequest: CreateEquipmentCoreRequest,
+  ): Promise<object> {
+    return await this.equipmentCoreService.createEquipment(
+      createEquipmentCoreRequest,
+    );
   }
 }
--- src/app/equipment/equipment-core/equipment-core.service.ts
+++ src/app/equipment/equipment-core/equipment-core.service.ts
@@ -1,96 +1,28 @@
-import { InjectRepository } from '@nestjs/typeorm';
-import { paginate, Pagination } from 'nestjs-typeorm-paginate';
-import { Repository } from 'typeorm';
-import { FindOptionsSelect } from 'typeorm/find-options/FindOptionsSelect';
-
-import { CreateEquipmentData } from '@app/equipment/equipment-core/commands/create-equipment.data';
-import { UpdateEquipmentData } from '@app/equipment/equipment-core/commands/update-equipment.data';
-import { EquipmentProfileResponse } from '@app/equipment/equipment-core/dtos/equipment-profile.response';
-import { Equipment } from '@domain/equipment/entities/equipment.entity';
-import {
-  DuplicatedEnTitleException,
-  DuplicatedTitleException,
-  EquipmentNotFoundException,
-} from '@domain/equipment/equipment.errors';
+import { CreateEquipmentCoreRequest } from '@app/equipment/equipment-core/dtos/create-equipment-core.request';
 
 export class EquipmentCoreService {
-  constructor(
-    @InjectRepository(Equipment)
-    private readonly equipmentRepository: Repository<Equipment>,
-  ) {}
+  async getEquipments(): Promise<object[]> {
+    return [
+      {
+        id: '1',
+        name: 'Equipment 1',
+        description: 'Description 1',
+      },
+      {
+        id: '2',
+        name: 'Equipment 2',
+        description: 'Description 2',
+      },
+    ];
+  }
 
-  async searchEquipment(
-    page: number,
-    limit: number,
-  ): Promise<Pagination<EquipmentProfileResponse>> {
-    const queryBuilder =
-      this.equipmentRepository.createQueryBuilder('equipment');
-
-    const { items, meta } = await paginate(queryBuilder, { page, limit });
-
+  async createEquipment(
+    createEquipmentCoreRequest: CreateEquipmentCoreRequest,
+  ): Promise<object> {
     return {
-      items: items.map((manual) => new EquipmentProfileResponse(manual)),
-      meta,
+      id: '3',
+      name: createEquipmentCoreRequest.name,
+      description: createEquipmentCoreRequest.description,
     };
-  }
-
-  async postEquipment(
-    data: CreateEquipmentData,
-  ): Promise<EquipmentProfileResponse> {
-    await this.validateTitles(data.title, data.enTitle);
-    const equipment = await this.equipmentRepository.save({ ...data });
-    return new EquipmentProfileResponse(equipment);
-  }
-
-  async updateEquipment(
-    id: string,
-    data: UpdateEquipmentData,
-  ): Promise<EquipmentProfileResponse> {
-    await this.validateTitles(data.title, data.enTitle);
-
-    const equipment = await this.equipmentRepository.findOne({ where: { id } });
-
-    const updatedEquipment = await this.equipmentRepository.save({
-      equipment,
-      ...data,
-    });
-
-    return new EquipmentProfileResponse(updatedEquipment);
-  }
-
-  async deleteEquipment(id: string): Promise<boolean> {
-    const equipment = await this.findById(id);
-
-    const { affected } = await this.equipmentRepository.softDelete(
-      equipment.id,
-    );
-
-    return affected > 0;
-  }
-
-  async findById(
-    id: string,
-    select?: FindOptionsSelect<Equipment>,
-  ): Promise<Equipment> {
-    const equipment = await this.equipmentRepository.findOne({
-      where: { id },
-      select,
-    });
-    if (!equipment) throw new EquipmentNotFoundException();
-    return equipment;
-  }
-
-  protected async validateTitles(
-    title: string,
-    enTitle: string,
-  ): Promise<void> {
-    await Promise.all([
-      this.equipmentRepository.count({ where: { title } }).then((count) => {
-        if (count > 0) throw new DuplicatedTitleException();
-      }),
-      this.equipmentRepository.count({ where: { enTitle } }).then((count) => {
-        if (count > 0) throw new DuplicatedEnTitleException();
-      }),
-    ]);
   }
 }
