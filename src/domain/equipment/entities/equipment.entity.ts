d7b31af89b8a46ac784d598c8482067e47d991b6
puleugo <puleugo@gmail.com>
2022-08-10 13:42:16 +0900

    test: 헬스 기구 Entity 구현

--- src/domain/equipment/entities/equipment.entity.ts
+++ src/domain/equipment/entities/equipment.entity.ts
@@ -3,41 +3,25 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
-  ManyToOne,
-  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
-import { EquipmentManual } from '@domain/equipment/entities/equipment-manual.entity';
-import { GymEquipment } from '@domain/gym/entities/gym-equipment.entity';
-
-@Entity('equipments')
+@Entity('equipment')
 export class Equipment {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
   @Column()
-  title!: string;
+  name!: string;
 
-  @Column()
-  enTitle!: string;
-
-  @Column({ type: String, nullable: true })
-  description!: string | null;
-
-  @OneToMany(() => EquipmentManual, ({ equipments }) => equipments)
-  manuals!: EquipmentManual[];
-
-  @ManyToOne(() => GymEquipment, ({ equipments }) => equipments)
-  gymEquipment!: GymEquipment;
+  @Column({ nullable: true })
+  description?: string | null;
 
   @CreateDateColumn()
   createdAt!: Date;
-
   @UpdateDateColumn()
   updatedAt!: Date;
-
   @DeleteDateColumn()
   deletedAt!: Date | null;
 }
