61bf3b1336dbb000d6c97aac96db8f6c7993bead
puleugo <puleugo@gmail.com>
2022-08-16 20:45:15 +0900

    init: 커뮤니티 게시글 스켈리톤 코드

--- src/app/community/post/post.controller.ts
+++ src/app/community/post/post.controller.ts
@@ -1,57 +1,38 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
-import { PostCreateRequest } from '@app/community/post/dtos/post-create.request';
-import { PostProfileUpdateRequest } from '@app/community/post/dtos/post-profile-update.request';
-import { PostProfileResponse } from '@app/community/post/dtos/post-profile.response';
 import { PostService } from '@app/community/post/post.service';
 
-@Controller('posts')
+@Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Get()
-  async getPosts(): Promise<PostProfileResponse[]> {
+  async getPosts() {
     return await this.postService.getPosts();
   }
 
-  @Get('/board/:boardId')
-  async getPostsByBoardId(
-    @Param('boardId') boardId: string,
-  ): Promise<PostProfileResponse[]> {
+  @Get('/:boardId')
+  async getPostsByBoardId(boardId: string) {
     return await this.postService.getPostsByBoardId(boardId);
   }
 
   @Get('/:postId')
-  async getPostById(@Param('postId') postId: string): Promise<object> {
+  async getPostById(postId: string) {
     return await this.postService.getPostById(postId);
   }
 
-  @Post('/board/:boardId')
-  async createPost(
-    @Param('boardId') boardId: string,
-    @Body() data: PostCreateRequest,
-  ): Promise<object> {
-    return await this.postService.createPost(boardId, data);
+  @Post()
+  async createPost() {
+    return await this.postService.createPost();
   }
 
   @Patch('/:postId')
-  async updatePost(
-    @Param('postId') postId: string,
-    @Body() data: PostProfileUpdateRequest,
-  ): Promise<object> {
-    return await this.postService.updatePost(postId, data);
+  async updatePost(postId: string) {
+    return await this.postService.updatePost(postId);
   }
 
   @Delete('/:postId')
-  async deletePost(postId: string): Promise<boolean> {
+  async deletePost(postId: string) {
     return await this.postService.deletePost(postId);
   }
 }
--- src/app/community/post/post.module.ts
+++ src/app/community/post/post.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PostController } from '@app/community/post/post.controller';
 import { PostService } from '@app/community/post/post.service';
-import { Post } from '@domain/community/entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [],
   controllers: [PostController],
   providers: [PostService],
 })
