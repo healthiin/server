1cddbf1de36a0d9f3c378dbcff0b784e0dedb9fd
puleugo <puleugo@gmail.com>
2022-08-19 18:23:54 +0900

    feat: 게시글 작성 기능 서비스 구현

--- src/app/community/post/post.service.ts
+++ src/app/community/post/post.service.ts
@@ -1,27 +1,75 @@
 export class PostService {
-  constructor() {}
+  // constructor() {}
 
-  async getPosts() {
-    return [];
+  async getPosts(): Promise<object[]> {
+    return [
+      {
+        id: '1',
+        title: '제목1',
+        content: '내용1',
+        boardId: { id: '1' },
+        createdAt: new Date(),
+      },
+      {
+        id: '2',
+        title: '제목2',
+        content: '내용2',
+        boardId: { id: '1' },
+        createdAt: new Date(),
+      },
+    ];
   }
 
-  async getPostsByBoardId(boardId: string) {
-    return [];
+  async getPostsByBoardId(boardId: string): Promise<object[]> {
+    return [
+      {
+        id: '1',
+        title: '제목1',
+        content: '내용1',
+        boardId: { id: '1' },
+        createdAt: new Date(),
+      },
+      {
+        id: '2',
+        title: '제목2',
+        content: '내용2',
+        boardId: { id: '1' },
+        createdAt: new Date(),
+      },
+    ];
   }
 
-  async getPostById(postId: string) {
-    return {};
+  async getPostById(postId: string): Promise<object> {
+    return {
+      id: '1',
+      title: '제목1',
+      content: '내용1',
+      boardId: { id: '1' },
+      createdAt: new Date(),
+    };
   }
 
-  async createPost() {
-    return {};
+  async createPost(): Promise<object> {
+    return {
+      id: '1',
+      title: '제목1',
+      content: '내용1',
+      boardId: { id: '1' },
+      createdAt: new Date(),
+    };
   }
 
-  async updatePost(postId: string) {
-    return {};
+  async updatePost(postId: string): Promise<object> {
+    return {
+      id: '1',
+      title: '제목1-수정',
+      content: '내용1-수정',
+      boardId: { id: '1' },
+      createdAt: new Date(),
+    };
   }
 
-  async deletePost(postId: string) {
+  async deletePost(postId: string): Promise<boolean> {
     return true;
   }
 }
