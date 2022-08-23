596ab28096cf1ce7dc3f42a7ed6334f85cf618f4
puleugo <puleugo@gmail.com>
2022-08-24 00:08:04 +0900

    feat: 마이그레이션 작업

---
+++ src/app/community/comment/dtos/comment-create.request.ts
@@ -0,0 +1,6 @@
+// import { Comment } from '@domain/community/entities/comment.entity';
+//
+// export class CommentCreateRequest {
+//   content!: string;
+//   parentCommentId?: Comment | null;
+// }
