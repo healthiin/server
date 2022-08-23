6de7d6e3fa7e610923e0578580cae1ca83fb831d
Junsu Lee <me@adrinerdp.co>
2022-08-24 00:16:44 +0900

    chore: 불필요한 파일 제거

--- src/app/community/comment/dtos/comment-create.request.ts
+++
@@ -1,6 +0,0 @@
-// import { Comment } from '@domain/community/entities/comment.entity';
-//
-// export class CommentCreateRequest {
-//   content!: string;
-//   parentCommentId?: Comment | null;
-// }
