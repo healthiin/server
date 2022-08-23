3db1668f0fd2c8339f02a4ad85fda1c00f4a660d
puleugo <puleugo@gmail.com>
2022-08-23 11:21:28 +0900

    feat: article service 삭제

--- src/app/article/commands/article-create.data.ts
+++
@@ -1,7 +0,0 @@
-export type ArticleCreateData = {
-  username: string;
-  title: string;
-  contents: string;
-  thumbnail: string | null;
-  category: 'general' | 'question' | 'record' | 'information';
-};
--- src/app/article/dtos/article-update.request.ts
+++
@@ -1,3 +0,0 @@
-import { ArticleCreateRequest } from '@app/article/dtos/article-create.request';
-
-export class ArticleUpdateRequest extends ArticleCreateRequest {}
