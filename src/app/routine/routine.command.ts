d6fc4173025c516dac0dd1d5c695a3e8bec01e30
puleugo <puleugo@gmail.com>
2022-09-05 20:29:28 +0900

    feat: 메뉴얼 추가 기능 구현

--- src/app/routine/routine.command.ts
+++ src/app/routine/routine.command.ts
@@ -1,16 +1,19 @@
 import { RoutineProperties } from '@domain/routine/routine';
 
 type RoutineInfo = { routineId: string };
+type manualIds = { manualIds: string[] };
+type UserInfo = { userId: string };
 
 export type RoutineListQuery = {
   page: number;
   limit: number;
 };
 
-export type RoutineCreateCommand = Pick<
-  RoutineProperties,
-  'description' | 'title'
->;
+export type RoutineCreateCommand =
+  // UserInfo &
+  manualIds & Pick<RoutineProperties, 'title' | 'description'>;
 
-export type RoutineUpdateCommand = Partial<RoutineProperties>;
+export type RoutineUpdateCommand =
+  // UserInfo &
+  RoutineInfo & Partial<RoutineCreateCommand>;
 export type RoutineDeleteCommand = RoutineInfo;
