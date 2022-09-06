3c1eaa942b7d73446c3314ab210cd9e27d68177d
puleugo <puleugo@gmail.com>
2022-09-07 06:40:05 +0900

    docs: equipment 스웨거 작성

--- src/app/equipment/equipment-core/dtos/equipment-create.request.ts
+++ src/app/equipment/equipment-core/dtos/equipment-create.request.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 import { EquipmentCreateCommand } from '@app/equipment/equipment-core/equipment-core.command';
 
 export class EquipmentCreateRequest implements EquipmentCreateCommand {
+  @ApiProperty({ example: '기구명' })
   @IsNotEmpty()
   @IsString()
   name!: string;
 
+  @ApiProperty({ example: '기구 영어명' })
   @IsNotEmpty()
   @IsString()
   enName!: string;
 
+  @ApiProperty({ example: '기구에 대한 설명' })
   @IsNotEmpty()
   @IsString()
   description!: string;
--- src/app/equipment/equipment-core/dtos/equipment-update.request.ts
+++ src/app/equipment/equipment-core/dtos/equipment-update.request.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 import { EquipmentUpdateCommand } from '@app/equipment/equipment-core/equipment-core.command';
@@ -5,14 +6,17 @@ import { EquipmentUpdateCommand } from '@app/equipment/equipment-core/equipment-
 export class EquipmentUpdateRequest
   implements Omit<EquipmentUpdateCommand, 'equipmentId'>
 {
+  @ApiProperty({ description: '기구명' })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({ description: '기구 영어명' })
   @IsOptional()
   @IsString()
   enName?: string;
 
+  @ApiProperty({ description: '기구에 대한 설명' })
   @IsOptional()
   @IsString()
   description?: string;
