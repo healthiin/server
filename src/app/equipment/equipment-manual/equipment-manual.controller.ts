a67e8dcb1df8f402d77289c921b1cff420558125
puleugo <puleugo@gmail.com>
2022-09-07 07:39:23 +0900

    docs: 운동 메뉴얼 문서화

--- src/app/equipment/equipment-manual/equipment-manual.controller.ts
+++ src/app/equipment/equipment-manual/equipment-manual.controller.ts
@@ -8,22 +8,35 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { ManualCreateRequest } from '@app/equipment/equipment-manual/dtos/manual-create.request';
 import { ManualProfileResponse } from '@app/equipment/equipment-manual/dtos/manual-profile.response';
 import { ManualUpdateRequest } from '@app/equipment/equipment-manual/dtos/manual-update.request';
 import { EquipmentManualService } from '@app/equipment/equipment-manual/equipment-manual.service';
+import { MANUAL_ERRORS } from '@domain/equipment/manual.errors';
 
 @Controller('manuals')
+@ApiTags('[헬스 기구] 설명서')
 export class EquipmentManualController {
   constructor(private readonly manualService: EquipmentManualService) {}
 
   @Get()
+  @ApiOperation({ summary: '모든 설명서를 조회합니다.' })
+  @ApiOkResponse({ type: [ManualProfileResponse] })
   async getAllManuals(): Promise<ManualProfileResponse[]> {
     return this.manualService.getAllManuals();
   }
 
   @Get('/:manualId')
+  @ApiOperation({ summary: '설명서 상세 정보를 조회합니다.' })
+  @ApiOkResponse({ type: ManualProfileResponse })
   async getManualById(
     @Param('manualId', ParseUUIDPipe) manualId: string,
   ): Promise<ManualProfileResponse> {
@@ -31,6 +44,8 @@ export class EquipmentManualController {
   }
 
   @Get('category/:category')
+  @ApiOperation({ summary: '카테고리별 설명서를 조회합니다.' })
+  @ApiOkResponse({ type: [ManualProfileResponse] })
   async getManualsByType(
     @Param('category')
     type: 'back' | 'shoulder' | 'chest' | 'arm' | 'lef' | 'abs',
@@ -39,6 +54,11 @@ export class EquipmentManualController {
   }
 
   @Post('/:equipmentId')
+  @ApiOperation({ summary: '특정 헬스 기구에 대한 설명서를 등록합니다.' })
+  @ApiCreatedResponse({ type: ManualProfileResponse })
+  @ApiNotFoundResponse({
+    description: MANUAL_ERRORS.EQUIPMENT_MANUAL_NOT_FOUND,
+  })
   async createManual(
     @Param('equipmentId', ParseUUIDPipe) equipmentId: string,
     @Body() data: ManualCreateRequest,
@@ -51,6 +71,11 @@ export class EquipmentManualController {
   }
 
   @Patch('/:manualId')
+  @ApiOperation({ summary: '특정 설명서를 수정합니다.' })
+  @ApiOkResponse({ type: ManualProfileResponse })
+  @ApiNotFoundResponse({
+    description: MANUAL_ERRORS.EQUIPMENT_MANUAL_NOT_FOUND,
+  })
   async updateManual(
     @Param('manualId', ParseUUIDPipe) manualId: string,
     @Body() data: ManualUpdateRequest,
@@ -64,6 +89,11 @@ export class EquipmentManualController {
   }
 
   @Delete('/:manualId')
+  @ApiOperation({ summary: '특정 설명서를 삭제합니다.' })
+  @ApiOkResponse({ type: Boolean })
+  @ApiNotFoundResponse({
+    description: MANUAL_ERRORS.EQUIPMENT_MANUAL_NOT_FOUND,
+  })
   async withdrawManual(
     @Param('manualId', ParseUUIDPipe) manualId: string,
   ): Promise<boolean> {
