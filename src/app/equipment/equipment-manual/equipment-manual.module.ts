e9d5639460ee1b832f3846a90e657a69bae1feb1
puleugo <puleugo@gmail.com>
2022-09-07 19:06:55 +0900

    fix: 의존성 문제 해결

--- src/app/equipment/equipment-manual/equipment-manual.module.ts
+++ src/app/equipment/equipment-manual/equipment-manual.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { EquipmentCoreModule } from '@app/equipment/equipment-core/equipment-core.module';
 import { EquipmentManualController } from '@app/equipment/equipment-manual/equipment-manual.controller';
 import { EquipmentManualService } from '@app/equipment/equipment-manual/equipment-manual.service';
 import { Manual } from '@domain/equipment/equipment-manual.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Manual])],
+  imports: [TypeOrmModule.forFeature([Manual]), EquipmentCoreModule],
   controllers: [EquipmentManualController],
   providers: [EquipmentManualService],
 })
