85d15de1acc4375c9763db4f25e98a9ab6e90c05
puleugo <puleugo@gmail.com>
2022-09-25 20:41:09 +0900

    fix: 1662545673155-release_v1_beta.ts 마이그레이션 복구

--- src/migrations/1662545673155-release_v1_beta.ts
+++ src/migrations/1662545673155-release_v1_beta.ts
@@ -1,9 +1,22 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class createUsersTable1664035962513 implements MigrationInterface {
-  name = 'createUsersTable1664035962513';
+export class createUsersTable1662545673155 implements MigrationInterface {
+  name = 'createUsersTable1662545673155';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "boards" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "title" character varying NOT NULL,
+                "description" character varying,
+                "slug" character varying,
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "deleted_at" TIMESTAMP,
+                CONSTRAINT "UQ_9a01141982175d5633687bcb47d" UNIQUE ("slug"),
+                CONSTRAINT "PK_606923b0b068ef262dfdcd18f44" PRIMARY KEY ("id")
+            )
+        `);
     await queryRunner.query(`
             CREATE TABLE "routines" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
@@ -98,30 +111,18 @@ export class createUsersTable1664035962513 implements MigrationInterface {
             CREATE TABLE "users" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "username" character varying NOT NULL,
-                "nickname" character varying,
-                "age_range" character varying,
-                "gender" character varying,
-                "user_email" character varying,
+                "password" character varying NOT NULL,
+                "name" character varying NOT NULL,
+                "nickname" character varying NOT NULL,
                 "avatar_image" character varying,
                 "is_admin" boolean NOT NULL DEFAULT false,
+                "phone_number" character varying NOT NULL,
                 "created_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "deleted_at" TIMESTAMP,
                 CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"),
                 CONSTRAINT "UQ_ad02a1be8707004cb805a4b5023" UNIQUE ("nickname"),
                 CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
-            )
-        `);
-    await queryRunner.query(`
-            CREATE TABLE "comments" (
-                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-                "content" character varying NOT NULL,
-                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
-                "deleted_at" TIMESTAMP,
-                "post_id" uuid,
-                "author_id" uuid,
-                CONSTRAINT "PK_8bf68bc960f2b69e818bdb90dcb" PRIMARY KEY ("id")
             )
         `);
     await queryRunner.query(`
@@ -138,16 +139,15 @@ export class createUsersTable1664035962513 implements MigrationInterface {
             )
         `);
     await queryRunner.query(`
-            CREATE TABLE "boards" (
+            CREATE TABLE "comments" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-                "title" character varying NOT NULL,
-                "description" character varying,
-                "slug" character varying,
+                "content" character varying NOT NULL,
                 "created_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "deleted_at" TIMESTAMP,
-                CONSTRAINT "UQ_9a01141982175d5633687bcb47d" UNIQUE ("slug"),
-                CONSTRAINT "PK_606923b0b068ef262dfdcd18f44" PRIMARY KEY ("id")
+                "post_id" uuid,
+                "author_id" uuid,
+                CONSTRAINT "PK_8bf68bc960f2b69e818bdb90dcb" PRIMARY KEY ("id")
             )
         `);
     await queryRunner.query(`
@@ -191,14 +191,6 @@ export class createUsersTable1664035962513 implements MigrationInterface {
             ADD CONSTRAINT "FK_60fd9bbcdd20224aba703ffbaf4" FOREIGN KEY ("gym_id") REFERENCES "gyms"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
     await queryRunner.query(`
-            ALTER TABLE "comments"
-            ADD CONSTRAINT "FK_259bf9825d9d198608d1b46b0b5" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "comments"
-            ADD CONSTRAINT "FK_e6d38899c31997c45d128a8973b" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
-        `);
-    await queryRunner.query(`
             ALTER TABLE "posts"
             ADD CONSTRAINT "FK_22fd59b98091387a27788f7a8b1" FOREIGN KEY ("board_id") REFERENCES "boards"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
@@ -206,20 +198,28 @@ export class createUsersTable1664035962513 implements MigrationInterface {
             ALTER TABLE "posts"
             ADD CONSTRAINT "FK_312c63be865c81b922e39c2475e" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
+    await queryRunner.query(`
+            ALTER TABLE "comments"
+            ADD CONSTRAINT "FK_259bf9825d9d198608d1b46b0b5" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+        `);
+    await queryRunner.query(`
+            ALTER TABLE "comments"
+            ADD CONSTRAINT "FK_e6d38899c31997c45d128a8973b" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+        `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            ALTER TABLE "posts" DROP CONSTRAINT "FK_312c63be865c81b922e39c2475e"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "posts" DROP CONSTRAINT "FK_22fd59b98091387a27788f7a8b1"
-        `);
     await queryRunner.query(`
             ALTER TABLE "comments" DROP CONSTRAINT "FK_e6d38899c31997c45d128a8973b"
         `);
     await queryRunner.query(`
             ALTER TABLE "comments" DROP CONSTRAINT "FK_259bf9825d9d198608d1b46b0b5"
+        `);
+    await queryRunner.query(`
+            ALTER TABLE "posts" DROP CONSTRAINT "FK_312c63be865c81b922e39c2475e"
+        `);
+    await queryRunner.query(`
+            ALTER TABLE "posts" DROP CONSTRAINT "FK_22fd59b98091387a27788f7a8b1"
         `);
     await queryRunner.query(`
             ALTER TABLE "gym_notices" DROP CONSTRAINT "FK_60fd9bbcdd20224aba703ffbaf4"
@@ -252,13 +252,10 @@ export class createUsersTable1664035962513 implements MigrationInterface {
             ALTER TABLE "routines" DROP CONSTRAINT "FK_8b95ddac537fa1fc1eddf7d4487"
         `);
     await queryRunner.query(`
-            DROP TABLE "boards"
+            DROP TABLE "comments"
         `);
     await queryRunner.query(`
             DROP TABLE "posts"
-        `);
-    await queryRunner.query(`
-            DROP TABLE "comments"
         `);
     await queryRunner.query(`
             DROP TABLE "users"
@@ -283,6 +280,9 @@ export class createUsersTable1664035962513 implements MigrationInterface {
         `);
     await queryRunner.query(`
             DROP TABLE "routines"
+        `);
+    await queryRunner.query(`
+            DROP TABLE "boards"
         `);
   }
 }
