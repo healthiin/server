58070d929b8a6441f988590aa87e169fe9fe18bd
Junsu Lee <me@adrinerdp.co>
2022-10-27 22:17:14 +0900

    ci: add e2e test

--- test/orm-config.ts
+++ test/orm-config.ts
@@ -1,39 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
-import { Board } from '@domain/community/board.entity';
-import { Comment } from '@domain/community/comment.entity';
-import { Post } from '@domain/community/post.entity';
-import { Manual } from '@domain/equipment/equipment-manual.entity';
-import { Equipment } from '@domain/equipment/equipment.entity';
-import { GymEquipment } from '@domain/gym/entities/gym-equipment.entity';
-import { GymNotice } from '@domain/gym/entities/gym-notice.entity';
-import { GymUser } from '@domain/gym/entities/gym-user.entity';
-import { Gym } from '@domain/gym/entities/gym.entity';
-import { Routine } from '@domain/routine/routine.entity';
-import { User } from '@domain/user/user.entity';
-
 const ormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'healthin_server',
-  password: 'secret',
-  database: 'healthin_test',
-  entities: [
-    User,
-    Gym,
-    GymUser,
-    GymNotice,
-    Post,
-    Comment,
-    Board,
-    Equipment,
-    GymEquipment,
-    Manual,
-    Routine,
-  ],
-  migrations: [__dirname, '../src/migration/*.ts'],
+  host: process.env.DB_HOST || 'postgres',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'healthin_server',
+  password: process.env.DB_PASSWORD || 'secret',
+  database: process.env.DB_DATABASE || 'healthin_test',
+  entities: ['**/*.entity.ts'],
   namingStrategy: new SnakeNamingStrategy(),
   dropSchema: true,
   logging: false,
