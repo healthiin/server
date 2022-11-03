87d635f2b87b1d3765b45e78684bfa227f011585
puleugo <puleugo@gmail.com>
2022-11-03 17:53:46 +0900

    docs: 댓글 조회 response 문서화

--- src/app/community/comment/dtos/comment-profile.response.ts
+++ src/app/community/comment/dtos/comment-profile.response.ts
@@ -14,28 +14,53 @@ export class CommentProfileResponse
       | 'childComment'
     >
 {
-  @ApiProperty()
+  @ApiProperty({
+    description: '댓글 ID입니다.',
+    example: 'f7f8d877-fe0d-4e46-a05b-cf73ca411746',
+  })
   id: string;
 
-  @ApiProperty({ description: '댓글 내용' })
+  @ApiProperty({ description: '댓글 내용입니다', example: '댓글 내용' })
   content: string;
 
-  @ApiProperty({ description: '게시글 ID' })
+  @ApiProperty({
+    description: '게시글 ID입니다',
+    example: '5a602b72-8192-4fc7-b5f8-a9e60f31674b',
+  })
   postId: string;
 
-  @ApiProperty({ description: '부모 댓글' })
-  replyId: string | null;
-
-  @ApiProperty({ description: '댓글 작성자 닉네임' })
+  @ApiProperty({
+    description: '댓글 작성자 닉네임입니다',
+    example: '댓글 작성자 닉네임',
+  })
   author: string;
 
-  @ApiProperty({ description: '대댓글' })
+  @ApiProperty({
+    description: '대댓글',
+    example: [
+      {
+        id: 'b5e15374-1859-450c-84e1-4846202bbd43',
+        content: 'test1-1',
+        createdAt: '2022-11-02T01:03:36.177Z',
+        updatedAt: '2022-11-02T01:03:36.177Z',
+        author: 'nickname2',
+        childComments: null,
+        postId: '5a602b72-8192-4fc7-b5f8-a9e60f31674b',
+      },
+    ],
+  })
   childComments: CommentProfileResponse[] | null;
 
-  @ApiProperty({ description: '댓글 작성일시' })
+  @ApiProperty({
+    description: '댓글 작성일시',
+    example: '2022-11-02T01:03:36.177Z',
+  })
   createdAt: Date;
 
-  @ApiProperty({ description: '댓글 수정일시' })
+  @ApiProperty({
+    description: '댓글 수정일시',
+    example: '2022-11-02T01:03:36.177Z',
+  })
   updatedAt: Date;
 
   constructor(data: CommentProperties) {
